934fdfa4bf971e62ccc0fc589a99c0ade9b04319
yuval0604 <bukovsky@mail.tau.ac.il>
2025-04-10 16:02:03 +0300

    Update symnmf.c

--- symnmf.c
+++ symnmf.c
@@ -7,6 +7,8 @@
 #define EPSILON 1e-4
 #define MAX_ITER 300
 #define BETA 0.5
+#define MAX_LINE_LEN 1024
+
 
 void print_error_and_exit(void) {
     printf("An Error Has Occurred\n");
@@ -41,20 +43,23 @@ double** allocate_matrix(int rows, int cols) {
     return matrix;
 }
 
-static double** matrix_mul(double** mat1, int n, double** mat2, int k) {
-    double** res = allocate_matrix(n, k);
-    int row, col, l;
-    for (row = 0; row < n; row++) {
-        for (col = 0; col < k; col++) {
-            double sum_res = 0.0;
-            for (l = 0; l < n; l++) {
-                sum_res += mat1[row][l] * mat2[l][col];
-                }
-            res[row][col] = sum_res;
+static double** matrix_mul(double** A, int rows, int inner, double** B, int cols) {
+    double** res = allocate_matrix(rows, cols);
+    int i, j, k;
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
+            double sum = 0.0;
+            for (k = 0; k < inner; k++) {
+                sum += A[i][k] * B[k][j];
+            }
+            res[i][j] = sum;
         }
     }
     return res;
 }
+
+
+
 
 static double** compute_HtH(double** H, int n, int k) {
     double** res = allocate_matrix(k, k);
@@ -165,9 +170,10 @@ double** symnmf_optimization(double** W, int n, int k, double** initial_H) {
 
     for (iter = 0; iter < MAX_ITER; iter++) {
         
-        double** WH = matrix_mul(W, n, H, k);
+        double** WH = matrix_mul(W, n, n, H, k);
         double** HtH = compute_HtH(H, n, k);
-        double** HHtH = matrix_mul(H, n, HtH, k);
+        double** HHtH = matrix_mul(H, n, k, HtH, k);
+
 
         for (i = 0; i < n; i++) {
             for (j = 0; j < k; j++) {
@@ -219,4 +225,110 @@ double** symnmf_optimization(double** W, int n, int k, double** initial_H) {
 }
 
 
+int main(int argc, char *argv[]) {
+    char *goal, *filename, *token;
+    FILE *fp;
+    int n=0;
+    int d=0;
+    int i=0;
+    int j = 0;
+    int r, c;
+    char line[MAX_LINE_LEN];
+    double **data, **A, **D; 
+    double **result_matrix=NULL;
+    
+    if (argc != 3) {
+        printf("An Error Has Occurred\n");
+        return 1;
+    }
+    
+    goal = argv[1];
+    filename = argv[2];
 
+    if (strcmp(goal, "sym") != 0 && strcmp(goal, "ddg") != 0 && strcmp(goal, "norm") != 0) {
+        printf("An Error Has Occurred\n");
+        return 1;
+    }
+
+    fp = fopen(filename, "r");
+    if (!fp) {
+        printf("An Error Has Occurred\n");
+        return 1;
+    }
+
+   
+    if (fgets(line, sizeof(line), fp) == NULL) {
+        printf("An Error Has Occurred\n");
+        fclose(fp);
+        return 1;
+    }
+   
+    n = 1; 
+    token = strtok(line, ",");
+    while (token != NULL) {
+        d++;
+        token = strtok(NULL, ",");
+    }
+    while (fgets(line, sizeof(line), fp) != NULL) {
+        n++;
+    }
+    fclose(fp);
+
+    data = allocate_matrix(n, d);
+    if (!data) {
+        printf("An Error Has Occurred\n");
+        return 1;
+    }
+
+    fp = fopen(filename, "r");
+    if (!fp) {
+        printf("An Error Has Occurred\n");
+        free_matrix(data, n);
+        return 1;
+    }
+    
+    while (fgets(line, sizeof(line), fp) != NULL && i < n) {
+        line[strcspn(line, "\n")] = '\0';
+        token = strtok(line, ",");
+        while (token != NULL && j < d) {
+            data[i][j] = atof(token);
+            j++;
+            token = strtok(NULL, ",");
+        }
+        i++;
+    }
+    fclose(fp);
+
+    if (strcmp(goal, "sym") == 0) {
+        result_matrix = calculate_similarity_matrix(data, n, d);
+    }
+    else if (strcmp(goal, "ddg") == 0) {
+        A = calculate_similarity_matrix(data, n, d);
+        result_matrix = calculate_diagonal_degree_matrix(A, n);
+        free_matrix(A, n);
+    }
+    else if (strcmp(goal, "norm") == 0) {
+        A = calculate_similarity_matrix(data, n, d);
+        D = calculate_diagonal_degree_matrix(A, n);
+        result_matrix = calculate_normalized_similarity(A, D, n);
+        free_matrix(A, n);
+        free_matrix(D, n);
+    }
+    free_matrix(data, n);
+
+    if (!result_matrix) {
+        printf("An Error Has Occurred\n");
+        return 1;
+    }
+
+    for (r = 0; r < n; r++) {
+        for (c = 0; c < n; c++) {
+            printf("%.4f", result_matrix[r][c]);
+            if (c < n - 1)
+                printf(",");
+        }
+        printf("\n");
+    }
+    free_matrix(result_matrix, n);
+    return 0;
+}
